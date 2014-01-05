3abc35b2db8184b6925525db99f2dcd3c002b216
Dnyarri <william.linna@kapsi.fi>
2014-01-05 14:07:35 +0200

    Added smoke effect to Fireball. Fireball now explodes when expiring

--- src/arkhados/spell/spells/embermage/Fireball.java
+++ src/arkhados/spell/spells/embermage/Fireball.java
@@ -28,16 +28,21 @@ import arkhados.spell.Spell;
 import arkhados.spell.buffs.AbstractBuff;
 import arkhados.util.NodeBuilder;
 import arkhados.util.UserDataStrings;
+import com.jme3.asset.AssetManager;
 import com.jme3.bullet.collision.shapes.SphereCollisionShape;
 import com.jme3.bullet.control.RigidBodyControl;
 import com.jme3.effect.ParticleEmitter;
 import com.jme3.effect.ParticleMesh;
 import com.jme3.effect.shapes.EmitterSphereShape;
+import com.jme3.light.PointLight;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
+import com.jme3.renderer.queue.RenderQueue;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
+import com.jme3.scene.control.LightControl;
 import com.jme3.scene.shape.Sphere;
 
 /**
@@ -45,6 +50,7 @@ import com.jme3.scene.shape.Sphere;
  * moderate damage. Has small knockback effect on hit.
  */
 public class Fireball extends Spell {
+
     {
         this.iconName = "fireball.png";
     }
@@ -76,8 +82,32 @@ public class Fireball extends Spell {
 
 class FireballBuilder extends NodeBuilder {
 
+    private ParticleEmitter createSmokeEmitter() {
+        final ParticleEmitter smoke = new ParticleEmitter("smoke-emitter", ParticleMesh.Type.Triangle, 300);
+        Material materialGray = new Material(NodeBuilder.assetManager, "Common/MatDefs/Misc/Particle.j3md");
+        materialGray.setTexture("Texture", NodeBuilder.assetManager.loadTexture("Effects/flame.png"));
+        smoke.setMaterial(materialGray);
+        smoke.setImagesX(2);
+        smoke.setImagesY(2);
+        smoke.setSelectRandomImage(true);
+        smoke.setStartColor(new ColorRGBA(0.5f, 0.5f, 0.5f, 1.0f));
+        smoke.setStartColor(new ColorRGBA(0.5f, 0.5f, 0.5f, 0.1f));
+        smoke.getParticleInfluencer().setInitialVelocity(Vector3f.ZERO);
+//        fire.getParticleInfluencer().setInitialVelocity(Vector3f.UNIT_Z.mult(10));
+//        fire.getParticleInfluencer().setVelocityVariation(0.5f);
+        smoke.setStartSize(2.0f);
+        smoke.setEndSize(6.0f);
+        smoke.setGravity(Vector3f.ZERO);
+        smoke.setLowLife(1f);
+        smoke.setHighLife(1.3f);
+        smoke.setParticlesPerSec(100);
+
+        smoke.setRandomAngle(true);
+        return smoke;
+    }
+
     private ParticleEmitter createFireEmitter() {
-        final ParticleEmitter fire = new ParticleEmitter("fire-emitter", ParticleMesh.Type.Triangle, 100);
+        final ParticleEmitter fire = new ParticleEmitter("fire-emitter", ParticleMesh.Type.Triangle, 200);
         Material materialRed = new Material(NodeBuilder.assetManager, "Common/MatDefs/Misc/Particle.j3md");
         materialRed.setTexture("Texture", NodeBuilder.assetManager.loadTexture("Effects/flame.png"));
         fire.setMaterial(materialRed);
@@ -87,13 +117,15 @@ class FireballBuilder extends NodeBuilder {
         fire.setStartColor(new ColorRGBA(0.95f, 0.150f, 0.0f, 1.0f));
         fire.setEndColor(new ColorRGBA(1.0f, 1.0f, 0.0f, 0.5f));
         fire.getParticleInfluencer().setInitialVelocity(Vector3f.ZERO);
-        fire.setStartSize(6.5f);
-        fire.setEndSize(0.5f);
+//        fire.getParticleInfluencer().setInitialVelocity(Vector3f.UNIT_Z.mult(10));
+//        fire.getParticleInfluencer().setVelocityVariation(0.5f);
+        fire.setStartSize(2.5f);
+        fire.setEndSize(1.0f);
         fire.setGravity(Vector3f.ZERO);
-        fire.setLowLife(0.2f);
-        fire.setHighLife(0.3f);
-        fire.setParticlesPerSec(40);
-        fire.getParticleInfluencer().setVelocityVariation(0.5f);
+        fire.setLowLife(0.1f);
+        fire.setHighLife(0.1f);
+        fire.setParticlesPerSec(100);
+
         fire.setRandomAngle(true);
         return fire;
     }
@@ -102,6 +134,8 @@ class FireballBuilder extends NodeBuilder {
         final Sphere sphere = new Sphere(32, 32, 1.0f);
 
         final Geometry projectileGeom = new Geometry("projectile-geom", sphere);
+        projectileGeom.setCullHint(Spatial.CullHint.Always);
+
         final Node node = new Node("projectile");
         node.attachChild(projectileGeom);
 
@@ -116,17 +150,28 @@ class FireballBuilder extends NodeBuilder {
         node.setUserData(UserDataStrings.IMPULSE_FACTOR, 0f);
 
         if (NodeBuilder.worldManager.isClient()) {
-            final ParticleEmitter fire = this.createFireEmitter();
+//            PointLight light = new PointLight();
+//            light.setRadius(30f);
+//            light.setPosition(new Vector3f(-100f, -100f, -100f));
+//            LightControl lightControl = new LightControl(light);
+//            node.addControl(lightControl);
+//
+//            worldManager.getWorldRoot().addLight(light);
 
+            final ParticleEmitter fire = this.createFireEmitter();
             node.attachChild(fire);
+
+            final ParticleEmitter smoke = this.createSmokeEmitter();
+            node.attachChild(smoke);
 
             node.addControl(new EntityEventControl());
             /**
              * Here we specify what happens on client side when fireball is
              * removed. In this case we want explosion effect.
              */
-            final FireballRemovalAction removalAction = new FireballRemovalAction();
-            removalAction.setEmitter(fire);
+            final FireballRemovalAction removalAction = new FireballRemovalAction(assetManager);
+            removalAction.setFireEmitter(fire);
+            removalAction.setSmokeTrail(smoke);
 
 
             node.getControl(EntityEventControl.class).setOnRemoval(removalAction);
@@ -161,26 +206,82 @@ class FireballBuilder extends NodeBuilder {
 class FireballRemovalAction implements RemovalEventAction {
 
     private ParticleEmitter fire;
+    private ParticleEmitter smokeTrail;
+    private AssetManager assetManager;
 
-    public void setEmitter(ParticleEmitter fire) {
+    public FireballRemovalAction(AssetManager assetManager) {
+        this.assetManager = assetManager;
+    }
+
+    public void setFireEmitter(ParticleEmitter fire) {
         this.fire = fire;
     }
 
+    private void leaveSmokeTrail(final Node worldRoot, Vector3f worldTranslation) {
+        this.smokeTrail.setParticlesPerSec(0);
+        worldRoot.attachChild(this.smokeTrail);
+        this.smokeTrail.setLocalTranslation(worldTranslation);
+        this.smokeTrail.addControl(new TimedExistenceControl(5f));
+    }
+
+    private void createSmokePuff(final Node worldRoot, Vector3f worldTranslation) {
+        final ParticleEmitter smokePuff = new ParticleEmitter("smoke-puff", ParticleMesh.Type.Triangle, 20);
+        Material materialGray = new Material(this.assetManager, "Common/MatDefs/Misc/Particle.j3md");
+        materialGray.setTexture("Texture", this.assetManager.loadTexture("Effects/flame.png"));
+        smokePuff.setMaterial(materialGray);
+        smokePuff.setImagesX(2);
+        smokePuff.setImagesY(2);
+        smokePuff.setSelectRandomImage(true);
+        smokePuff.setStartColor(new ColorRGBA(0.5f, 0.5f, 0.5f, 0.2f));
+        smokePuff.setStartColor(new ColorRGBA(0.5f, 0.5f, 0.5f, 0.1f));
+
+        smokePuff.getParticleInfluencer().setInitialVelocity(Vector3f.UNIT_X.mult(5.0f));
+        smokePuff.getParticleInfluencer().setVelocityVariation(1f);
+
+        smokePuff.setStartSize(2.0f);
+        smokePuff.setEndSize(6.0f);
+        smokePuff.setGravity(Vector3f.ZERO);
+        smokePuff.setLowLife(0.75f);
+        smokePuff.setHighLife(1f);
+        smokePuff.setParticlesPerSec(0);
+
+        smokePuff.setRandomAngle(true);
+
+        smokePuff.setShape(new EmitterSphereShape(Vector3f.ZERO, 4.0f));
+        worldRoot.attachChild(smokePuff);
+        smokePuff.setLocalTranslation(worldTranslation);
+        smokePuff.emitAllParticles();
+    }
+
     public void exec(WorldManager worldManager, String reason) {
-        if (!"collision".equals(reason)) {
-            return;
-        }
         Vector3f worldTranslation = fire.getParent().getLocalTranslation();
+        this.leaveSmokeTrail(worldManager.getWorldRoot(), worldTranslation);
+        this.createSmokePuff(worldManager.getWorldRoot(), worldTranslation);
+
         fire.removeFromParent();
         worldManager.getWorldRoot().attachChild(fire);
         fire.setLocalTranslation(worldTranslation);
-        fire.addControl(new TimedExistenceControl(0.3f));
-        fire.getParticleInfluencer().setInitialVelocity(Vector3f.UNIT_X.mult(15.0f));
-        fire.setShape(new EmitterSphereShape(Vector3f.ZERO, 6.0f));
+        fire.addControl(new TimedExistenceControl(1f));
+
+        fire.setStartColor(new ColorRGBA(0.95f, 0.150f, 0.0f, 0.40f));
+        fire.setEndColor(new ColorRGBA(1.0f, 1.0f, 0.0f, 0.0f));
+        fire.setLowLife(0.05f);
+        fire.setHighLife(0.15f);
+        fire.setNumParticles(100);
+        fire.setStartSize(0.50f);
+        fire.setEndSize(3.0f);
+        fire.getParticleInfluencer().setInitialVelocity(Vector3f.UNIT_X.mult(25.0f));
+        fire.getParticleInfluencer().setVelocityVariation(1f);
+
+        fire.setShape(new EmitterSphereShape(Vector3f.ZERO, 2.0f));
         fire.emitAllParticles();
         fire.setParticlesPerSec(0.0f);
 
         // TODO: Add soundeffect too!
+    }
+
+    public void setSmokeTrail(ParticleEmitter smoke) {
+        this.smokeTrail = smoke;
     }
 }
 
