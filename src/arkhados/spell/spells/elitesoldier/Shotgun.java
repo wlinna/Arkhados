e0ce5ddee0f8da7434a687d6f252b2e395913121
William Linna <william.linna@kapsi.fi>
2014-04-05 13:53:17 +0300

    Shotgun's damage / pellet is now 30 instead of 150

--- src/arkhados/spell/spells/elitesoldier/Shotgun.java
+++ src/arkhados/spell/spells/elitesoldier/Shotgun.java
@@ -173,7 +173,7 @@ class PelletBuilder extends NodeBuilder {
 
         node.setUserData(UserDataStrings.SPEED_MOVEMENT, 140f);
         node.setUserData(UserDataStrings.MASS, 0.30f);
-        node.setUserData(UserDataStrings.DAMAGE, 150f);
+        node.setUserData(UserDataStrings.DAMAGE, 30f);
         node.setUserData(UserDataStrings.IMPULSE_FACTOR, 0f);
 
         if (NodeBuilder.worldManager.isClient()) {
