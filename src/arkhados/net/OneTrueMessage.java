b6f05bf542402b60d756f5cbaf6f67d5583b96f4
william <william@localhost.localdomain>
2014-06-19 22:23:47 +0300

    Started working towards guaranteed and batched UDP messaging

---
+++ src/arkhados/net/OneTrueMessage.java
@@ -0,0 +1,49 @@
+/*    This file is part of Arkhados.
+
+ Arkhados is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Arkhados is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Arkhados.  If not, see <http://www.gnu.org/licenses/>. */
+package arkhados.net;
+
+import com.jme3.network.AbstractMessage;
+import com.jme3.network.serializing.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author william
+ */
+
+@Serializable
+public class OneTrueMessage extends AbstractMessage {
+    private int orderNum;
+    transient private int confirmsUnder;
+    
+    /**
+     * Guaranteed messages must be handled first
+     */
+    private List<Command> guaranteed = new ArrayList<>();
+    private List<Command> unreliables = new ArrayList<>();
+
+    public int getOrderNum() {
+        return orderNum;
+    }
+
+    public List<Command> getGuaranteed() {
+        return guaranteed;
+    }
+
+    public List<Command> getUnreliables() {
+        return unreliables;
+    }
+}
