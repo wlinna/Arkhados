5dab48c24d8d34d630febacf390a966d241a46c9
William Linna <william.linna@kapsi.fi>
2015-03-17 08:39:59 +0200

    Corrected SlowCC

--- src/arkhados/spell/buffs/SlowCC.java
+++ src/arkhados/spell/buffs/SlowCC.java
@@ -14,50 +14,25 @@
  along with Arkhados.  If not, see <http://www.gnu.org/licenses/>. */
 package arkhados.spell.buffs;
 
-import arkhados.controls.InfluenceInterfaceControl;
 import arkhados.util.BuffTypeIds;
-import arkhados.util.UserDataStrings;
-import com.jme3.scene.Spatial;
 
 /**
  *
  * @author william
  */
 public class SlowCC extends CrowdControlBuff {
-    {
+       {
         setTypeId(BuffTypeIds.SLOW);
     }
 
     private float slowFactor;
-    private Spatial spatial;
 
     public SlowCC(int id, float duration, float slow) {
         super(id, duration);
-        this.slowFactor = 1f - slow;
+        slowFactor = 1f - slow;
     }
-
-    @Override
-    public void attachToCharacter(InfluenceInterfaceControl influenceInterface) {
-        super.attachToCharacter(influenceInterface);
-        this.spatial = influenceInterface.getSpatial();
-    }
-
-
 
     public float getSlowFactor() {
-        return this.slowFactor;
+        return slowFactor;
     }
-
-    @Override
-    public void update(float time) {
-        super.update(time);
-        if (this.targetInterface.isSpeedConstant()) {
-            return;
-        }
-        Float msCurrent = this.spatial.getUserData(UserDataStrings.SPEED_MOVEMENT);
-        msCurrent *= this.slowFactor;
-        this.spatial.setUserData(UserDataStrings.SPEED_MOVEMENT, msCurrent);
-    }
-
-
 }
