d5e29f91b334c61dcb7f769af0d88a260566a3be
William Linna <william.linna@openmailbox.org>
2015-05-10 02:17:03 +0300

    Nerfed RockGolem's health 2400->2100

--- src/arkhados/characters/RockGolem.java
+++ src/arkhados/characters/RockGolem.java
@@ -75,7 +75,7 @@ public class RockGolem extends AbstractNodeBuilder {
         entity.setUserData(UserDataStrings.SPEED_ROTATION, 0.0f);
         float radius = 5.0f;
         entity.setUserData(UserDataStrings.RADIUS, radius);
-        float health = 2400f;
+        float health = 2100f;
         entity.setUserData(UserDataStrings.HEALTH_MAX, health);
         entity.setUserData(UserDataStrings.HEALTH_CURRENT, health);
         if (params.age < 0f) {
