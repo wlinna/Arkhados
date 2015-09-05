c358971b18eff880606e5868eb880d940904765b
William Linna <william.linna@openmailbox.org>
2015-09-05 16:58:12 +0300

    Renamed textures and removed some unneeded files

--- src/arkhados/arena/PillarArena.java
+++ src/arkhados/arena/PillarArena.java
@@ -53,7 +53,7 @@ public class PillarArena extends AbstractArena {
 
         Vector3f extent = ((BoundingBox) getTerrainNode().getWorldBound())
                 .getExtent(new Vector3f());
-        radius = extent.x - 15;
+        radius = extent.x;
 
         if (worldManager.isClient()) {
             createLavaQuad();
