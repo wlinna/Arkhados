e6395f03e2397159f706fab9dab2785b79bbab9e
William Linna <william.linna@openmailbox.org>
2016-04-16 00:15:17 +0300

    Replays are now compressed

--- src/arkhados/replay/ReplaySerializer.java
+++ src/arkhados/replay/ReplaySerializer.java
@@ -15,11 +15,17 @@
 package arkhados.replay;
 
 import com.jme3.network.serializing.Serializer;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.zip.DataFormatException;
+import java.util.zip.Deflater;
+import java.util.zip.Inflater;
 
 public class ReplaySerializer extends Serializer {
 
@@ -37,10 +43,10 @@ public class ReplaySerializer extends Serializer {
 
         String version = stringSerializer.readObject(data, String.class);
         replayData.getHeader().setVersion(version);
-        
+
         String gameMode = stringSerializer.readObject(data, String.class);
         replayData.getHeader().setGameMode(gameMode);
-        
+
         String arena = stringSerializer.readObject(data, String.class);
         replayData.getHeader().setArena(arena);
 
@@ -49,13 +55,37 @@ public class ReplaySerializer extends Serializer {
 
         byte flag = data.get();
 
-        ArrayList replayCmds = seqSerializer.readObject(data, ArrayList.class);
-        replayData.getCommands().addAll(replayCmds);
+        if (flag == 0) {
+            ArrayList replayCmds = seqSerializer.readObject(data, ArrayList.class);
+            replayData.getCommands().addAll(replayCmds);
+        } else if (flag == 1) {
+            int decompressedLength = data.getInt();
+            
+            byte[] compressedBytes = new byte[data.remaining()];
+            data.get(compressedBytes);
+            Inflater inf = new Inflater();
+            inf.setInput(compressedBytes, 0, compressedBytes.length);
+            byte[] decompressedBytes = new byte[decompressedLength];
+            try {
+                if (inf.inflate(decompressedBytes) != decompressedLength) {
+                    throw new AssertionError();
+                }
+            } catch (DataFormatException ex) {
+                Logger.getLogger(ReplaySerializer.class.getName())
+                        .log(Level.SEVERE, null, ex);
+            }
+            
+            inf.end();
+            
+            ByteBuffer decompressedBuffer = ByteBuffer.wrap(decompressedBytes);
+            ArrayList replayCmds = seqSerializer
+                    .readObject(decompressedBuffer, ArrayList.class);
+            replayData.getCommands().addAll(replayCmds);
+        }
 
         return replayData;
     }
 
-    
     @Override
     public void writeObject(ByteBuffer buffer, Object object) throws IOException {
         if (!(object instanceof ReplayData)) {
@@ -69,17 +99,41 @@ public class ReplaySerializer extends Serializer {
         Serializer mapSerializer = getExactSerializer(HashMap.class);
         Serializer seqSerializer = getExactSerializer(ArrayList.class);
 
-        dateSerializer.writeObject(buffer, replayData.getHeader().getDate());               
-        
+        dateSerializer.writeObject(buffer, replayData.getHeader().getDate());
+
         stringSerializer.writeObject(buffer, replayData.getHeader().getVersion());
-        stringSerializer.writeObject(buffer, replayData.getHeader().getGameMode());        
-        stringSerializer.writeObject(buffer, replayData.getHeader().getArena());        
-        
+        stringSerializer.writeObject(buffer, replayData.getHeader().getGameMode());
+        stringSerializer.writeObject(buffer, replayData.getHeader().getArena());
+
         mapSerializer.writeObject(buffer, replayData.getHeader().getPlayers());
 
-        byte flag = 0;
+        byte flag = 1; // Compression on (mode 1)
         buffer.put(flag);
 
-        seqSerializer.writeObject(buffer, replayData.getCommands());
+        ByteBuffer sequenceBuffer = ByteBuffer.allocate(314572800);
+
+        seqSerializer.writeObject(sequenceBuffer, replayData.getCommands());
+        sequenceBuffer.flip();
+
+        byte[] sequenceOutput = new byte[sequenceBuffer.remaining()];
+        sequenceBuffer.get(sequenceOutput);
+        
+        buffer.putInt(sequenceOutput.length);
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        Deflater def = new Deflater(Deflater.BEST_SPEED);
+        def.setInput(sequenceOutput);
+        def.finish();
+
+        byte[] buf = new byte[8192];
+        while (!def.finished()) {
+            int byteCount = def.deflate(buf);
+            out.write(buf, 0, byteCount);
+        }
+
+        def.end();
+        byte[] compressedBytes = out.toByteArray();
+
+        buffer.put(compressedBytes);
     }
 }
